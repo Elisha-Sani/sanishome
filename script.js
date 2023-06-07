6beedebbeb1ea23e1becbb2d89e6faf51217e8b9
Elisha-Sani <elishamuuo1@gmail.com>
2023-06-07 21:06:41 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector(".navbar");
 
@@ -18,3 +19,32 @@ document.querySelector("#search-icon").onclick = () => {
 document.querySelector("#close").onclick = () => {
     document.querySelector("#search-form").classList.remove("active");
 }
+
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+});
