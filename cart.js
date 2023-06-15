f19bc9d059cc5c4df858df904cddc590e490782b
Elisha-Sani <elishamuuo1@gmail.com>
2023-06-15 15:36:00 +0300

    ncom

---
+++ cart.js
@@ -0,0 +1,66 @@
+// Get the elements from the DOM
+var quantityInputs = document.getElementsByClassName('cart-quantity');
+var priceElements = document.getElementsByClassName('cart-price');
+var totalElement = document.querySelector('.total-price');
+var cartContent = document.querySelector('.cart-content');
+
+// Function to handle the remove button click
+function removeItem(event) {
+  var removeButton = event.target;
+  var cartBox = removeButton.parentNode;
+  cartBox.remove();
+
+  updateTotal();
+}
+
+// Event listener to handle remove button clicks
+var removeButtons = document.getElementsByClassName('cart-remove');
+for (var i = 0; i < removeButtons.length; i++) {
+  removeButtons[i].addEventListener('click', removeItem);
+}
+
+// Function to update the total price
+function updateTotal() {
+  var total = 0;
+
+  // Loop through each product in the cart
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var quantity = parseInt(quantityInputs[i].value);
+    var price = parseFloat(priceElements[i].textContent.replace('KES ', ''));
+    var productTotal = quantity * price;
+
+    total += productTotal;
+  }
+
+  totalElement.textContent = 'KES ' + total.toFixed(2);
+}
+
+// Event listener to update the total when quantity inputs change
+for (var i = 0; i < quantityInputs.length; i++) {
+  quantityInputs[i].addEventListener('change', updateTotal);
+}
+
+// Call the updateTotal function initially to calculate the total
+updateTotal();
+
+// Get selected products from local storage
+var selectedProducts = JSON.parse(localStorage.getItem('selectedProducts'));
+
+// Update cart with selected products
+if (selectedProducts && selectedProducts.length > 0) {
+  selectedProducts.forEach(productId => {
+    // Perform logic to add selected products to the cart
+    // You can use the productId to fetch product details from a database or an array of products
+    // and dynamically generate HTML elements to display the product information in the cartContent element
+
+    // Example code to add a product to the cart
+    var cartBox = document.createElement('div');
+    cartBox.classList.add('cart-box');
+
+    // Add product details to the cart box
+    // ...
+
+    // Append the cart box to the cart content
+    cartContent.appendChild(cartBox);
+  });
+}
--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-
-let menu = document.querySelector("#menu-bars");
-let navbar = document.querySelector(".navbar");
-
-menu.onclick = () => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-}
-
-let section = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header .navbar a');
-
-window.onscroll = () => {
-
-    menu.classList.remove("fa-times");
-    navbar.classList.remove("active");
-
-    section.forEach(sec => {
-        let top = window.scrollY;
-        let height = sec.offsetHeight;
-        let offset = sec.offsetTop - 150;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
-            });
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector("#menu-bars");
+    const navbar = document.querySelector(".navbar");
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('header .navbar a');
+    const cartIcon = document.querySelector("#cart-icon");
+    const cartQuantity = document.querySelector("#cart-icon .quantity");
+    let cartCount = 0;
+    let selectedProducts = [];
+  
+    // Toggle the menu and navbar
+    menu.addEventListener('click', () => {
+      menu.classList.toggle("fa-times");
+      navbar.classList.toggle("active");
+    });
+  
+    // Set the active link based on scroll position
+    window.addEventListener('scroll', () => {
+      let currentSectionId = '';
+  
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        if (window.scrollY >= sectionTop - 150 && window.scrollY < sectionTop + sectionHeight - 150) {
+          currentSectionId = section.getAttribute('id');
         }
-    })
-}
+      });
+  
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${currentSectionId}`) {
+          link.classList.add('active');
+        }
+      });
+    });
+  
+    // Add product to cart on click
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        cartCount++;
+        cartQuantity.textContent = cartCount;
+  
+        const productId = button.getAttribute('data-product-id');
+        selectedProducts.push(productId);
+        localStorage.setItem('selectedProducts', JSON.stringify(selectedProducts));
+      });
+    });
+  });
+  
+  
 
 document.querySelector("#search-icon").onclick = () => {
     document.querySelector("#search-form").classList.toggle("active");   
@@ -67,14 +87,4 @@ const swiper = new Swiper(".mySwiper", {
       },
 });
 
-/* 
-function loader() {
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
 
-function fadeOut() {
-    setInterval(loader, 1000);
-}
-
-window.onload = fadeOut;
-*/
